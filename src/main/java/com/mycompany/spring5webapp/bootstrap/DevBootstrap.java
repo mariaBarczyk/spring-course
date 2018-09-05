b73dead92e11157891d3c5c756e68e0387ac3095
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-05 13:23:39 +0200

    Update db initialization of Publisher data

--- src/main/java/com/mycompany/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/mycompany/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.mycompany.spring5webapp.bootstrap;
 
 import com.mycompany.spring5webapp.model.Author;
 import com.mycompany.spring5webapp.model.Book;
+import com.mycompany.spring5webapp.model.Publisher;
 import com.mycompany.spring5webapp.repository.AuthorRepository;
 import com.mycompany.spring5webapp.repository.BookRepository;
+import com.mycompany.spring5webapp.repository.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,13 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
         this.bookRepository = bookRepository;
+
     }
 
     @Override
@@ -27,19 +32,28 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initializeData() {
 
         Author hanna = new Author("Hanna", "Brown");
-        Book algorithms = new Book("Algorithms", "123", "Paul Fox");
+        Publisher paul = new Publisher("Paul","Roseville 19");
+        Book algorithms = new Book("Algorithms", "123", paul);
+
         hanna.getBooks().add(algorithms);
         algorithms.getAuthors().add(hanna);
 
         authorRepository.save(hanna);
+        publisherRepository.save(paul);
         bookRepository.save(algorithms);
 
+
+
         Author derek  = new Author("Derek", "Geller");
-        Book dataStructures = new Book("Data Structures", "158", "Paul Fox");
+        Publisher megan  = new Publisher("Megan","Santa Rosa 21");
+        Book dataStructures = new Book("Data Structures", "158", megan);
+
         derek.getBooks().add(dataStructures);
         dataStructures.getAuthors().add(derek);
 
         authorRepository.save(derek);
+        publisherRepository.save(megan);
         bookRepository.save(dataStructures);
+
     }
 }
