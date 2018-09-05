cf1f20e811b448aadc5fef987c4992e07256c7fe
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-05 11:13:02 +0200

    Add hibernate annotations to Book model

--- src/main/java/com/mycompany/spring5webapp/model/Book.java
+++ src/main/java/com/mycompany/spring5webapp/model/Book.java
@@ -1,14 +1,20 @@
 package com.mycompany.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
     private Set<Author> authors = new HashSet<>();
 
     public Book() {
@@ -23,6 +29,14 @@ public class Book {
     public Book(String title, String isbn, String publisher, Set<Author> authors) {
         this(title, isbn, publisher);
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
