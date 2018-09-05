496aa2afdd70aee3ab22c394e13358b4a762e444
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-05 12:04:40 +0200

    Create interfaces to book and author repositories

---
+++ src/main/java/com/mycompany/spring5webapp/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.mycompany.spring5webapp.repository;
+
+import com.mycompany.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
