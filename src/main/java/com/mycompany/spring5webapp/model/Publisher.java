088b76a0b0348f85ffe6d7208e883478ef1a4b42
mariaBarczyk <barczyk.maria92@gmail.com>
2018-09-05 13:13:06 +0200

    Create Publisher model

---
+++ src/main/java/com/mycompany/spring5webapp/model/Publisher.java
@@ -0,0 +1,49 @@
+package com.mycompany.spring5webapp.model;
+
+
+public class Publisher {
+
+    private Long id;
+    private String name;
+    private String address;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
